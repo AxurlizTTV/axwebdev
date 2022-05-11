7a426099a6d0d08e49c1797b42533737e91bc918
Axurliz <97619525+AxurlizTTV@users.noreply.github.com>
2022-05-11 11:33:19 +0200

    change port

--- server/index.js
+++ server/index.js
@@ -8,6 +8,6 @@ const app = express();
 app.use(bodyParser.json);
 app.use(cors());
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 80;
 
 app.listen(port, () => console.log(`Server started on port ${port}`)); 
